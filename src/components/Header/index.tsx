9899b50ab028d8b94a4c2161c8664fedff7d4e5b
Thomás Escolástico Bueno <62290004+thomasbueno@users.noreply.github.com>
2022-09-04 11:16:36 -0300

    improving logo on a button to open the drawer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
 
 import { Logo } from "./Logo";
 import { Search } from "./Search";
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
+import { useSideBarDrawer } from "../../contexts/SideBarDrawerContext";
+import { RiMenuLine } from "react-icons/ri";
 
 export function Header() {
+  const { onOpen } = useSideBarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -22,6 +26,16 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          icon={<Icon as={RiMenuLine} />}
+          aria-label="Open navigation"
+          fontSize="24"
+          mr="2"
+          variant="unstyled"
+          onClick={onOpen}
+        ></IconButton>
+      )}
       <Logo />
       {isWideVersion && <Search />}
 
