285bda6c98d5680ae61da1e2307ba97f352224ff
Thomás Escolástico Bueno <62290004+thomasbueno@users.noreply.github.com>
2022-09-04 21:13:12 -0300

    fix type of error

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -10,11 +10,11 @@ import { FieldError } from "react-hook-form";
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
-  error: FieldError;
+  error?: Partial<FieldError>;
 }
 
 const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
-  { name, label, error, ...rest },
+  { name, label, error = null, ...rest },
   ref
 ) => {
   return (
